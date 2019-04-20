ef183abd00a1a1bd430d25444d23ad053be3c233
Kent Tokunaga <KentTokunaga@gmail.com>
2019-04-20 17:37:16 -0500

    Using brew doctor to test

--- linux-setup.sh
+++ linux-setup.sh
@@ -10,13 +10,10 @@ sudo apt upgrade -y
 # added -y flag to answer yes to confirmations
 sudo apt install -y build-essential curl file git
 echo "/n" | sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
-test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
 sudo apt install -y linuxbrew-wrapper
-echo 'export PATH="/home/linuxbrew/.linuxbrew/sbin:$PATH"'
-export PATH="/home/linuxbrew/.linuxbrew/sbin:$PATH"
+echo 'export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"' >> ~/.profile
+export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+brew doctor
 
 ## install gcc
 brew install gcc
@@ -27,8 +24,12 @@ brew install git
 ## install azure-cli
 brew install azure-cli
 
-## install node
-#  installing python3-distutils because of issues with icu4c
-#  https://github.com/Homebrew/linuxbrew-core/issues/12680
-sudo apt install -y python3-distutils
-brew install node
+# ## install node
+# #  installing python3-distutils because of issues with icu4c
+# #  https://github.com/Homebrew/linuxbrew-core/issues/12680
+# sudo apt install -y python3-distutils
+# brew install node
+
+echo 'export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"' >> ~/.profile
+export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+brew doctor
