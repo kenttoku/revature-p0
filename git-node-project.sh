16d346e4dfb3e2c4f7d8af94d9875c116dcf4e8f
Kent Tokunaga <KentTokunaga@gmail.com>
2019-04-20 18:04:49 -0500

    Clarify error messages

--- git-node-project.sh
+++ git-node-project.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
-# verify dev environment
-
-## check for brew
+## verify dev environment
+source ~/.profile
 brew=$(which brew)
 node=$(which node)
 git=$(which git)
 az=$(which az)
 
-echo $brew
-echo $node
-echo $git
-echo $az
+## check for brew
+if [ -z $brew ]; then
+  echo "No brew. Please install before continuing" 1>&2
+  exit 1
+fi
+
+## check for node
+if [ -z $node ]; then
+  echo "No node. Please install before continuing" 1>&2
+  exit 2
+fi
+
+## check for git
+if [ -z $git ]; then
+  echo "No git. Please install before continuing" 1>&2
+  exit 3
+fi
+
+## check for azure-cli
+if [ -z $az ]; then
+  echo "No azure-cli. Please install before continuing" 1>&2
+  exit 4
+fi
