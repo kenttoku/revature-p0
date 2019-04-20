d6caebce61a372ac27e0aa7455b2af5b8386d9eb
Kent Tokunaga <KentTokunaga@gmail.com>
2019-04-20 18:33:48 -0500

    Create file strucutre

--- git-node-project.sh
+++ git-node-project.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+## equirement 2-
+## automate the process of setting up a new git project repository structure
+## include: verify req-1 is valid, create web node-based project (see week-3 revaturexyz.sh)
+## name: git-node-project.sh
+
 ## verify dev environment
 source ~/.profile
 brew=$(which brew)
@@ -30,3 +35,56 @@ if [ -z $az ]; then
   echo "No azure-cli. Please install before continuing" 1>&2
   exit 4
 fi
+
+## create git project
+dest=$1
+
+if [ -z $dest ]; then
+  dest=git-node-project
+fi
+
+mkdir $dest
+cd $dest
+git init
+
+## make it a node project
+npm init -y
+
+## create all directories
+mkdir -p \
+  .docker \
+  .github/ISSUE_TEMPLATE \
+  .github/PULL_REQUEST_TEMPLATE \
+  client \
+  src \
+  test
+
+## create files in main directory
+touch \
+  .azureup.yaml \
+  .dockerignore \
+  .editorconfig \
+  .gitignore \
+  .markdownlint.yaml \
+  CHANGELOG.md \
+  LICENSE.txt \
+  README.md
+
+## create files in directories
+### .docker
+touch \
+  .docker/dockerfile \
+  .docker/dockerup.yaml
+
+### .github
+touch \
+  .github/CODE-OF-CONDUCT.md \
+  .github/CONTRIBUTING.md \
+  .github/ISSUE_TEMPLATE/issue-template.md \
+  .github/PULL_REQUEST_TEMPLATE/pull-request-template.md \
+
+### others
+touch \
+  client/.gitkeep \
+  src/.gitkeep \
+  test/.gitkeep
