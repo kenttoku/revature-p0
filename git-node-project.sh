fc8e274aa342bffb70566bca84e93d7e427dae48
Kent Tokunaga <KentTokunaga@gmail.com>
2019-04-20 14:31:41 -0500

    Add eval to brew install process

--- git-node-project.sh
+++ git-node-project.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+# verify dev environment
+
+## check for brew
+brew=$(brew --version)
+
+echo brew
--- linux-setup.sh
+++ linux-setup.sh
@@ -12,16 +12,15 @@ sudo apt-get install -y build-essential curl file git
 
 # piping echo to answer prompt from brew installation
 echo "/n" | sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
-test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
 echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+eval \$($(brew --prefix)/bin/brew shellenv)
+brew --version
 
-## install gcc
-brew install gcc
+# ## install gcc
+# brew install gcc
 
-## install node
-brew install node
+# ## install node
+# brew install node
 
-# install azure-cli
-brew install azure-cli
+# ## install azure-cli
+# brew install azure-cli
