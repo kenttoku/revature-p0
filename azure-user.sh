45b9d5f8f8a5f8a0c45ebd0b73ac941dfeb58118
Kent Tokunaga <KentTokunaga@gmail.com>
2019-04-20 19:35:30 -0500

    Add instructions

--- azure-user.sh
+++ azure-user.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+# automate the process of creating, assigning, deleting a directory user
+# include: azure, must be admin,
+# add role of reader or contributor to subscription,
+# remove role of reader or contributor to subscription, delete non-admin only,
+# 1 script with 3 functions
+source ~/.profile
+
+## variables and constants
+DOMAIN=kenttokunagagmail.onmicrosoft.com
+PASSWORD=revature2019!
+username=$1
+userdisplayname=$3
+usersubscription=$4
+userprincipalname=$userdisplayname@$DOMAIN
+
+# login
+az login -u $username
+
+# get list of admins
+az role assignment list \
+  --include-classic-administrators \
+  --query "[?id=='NA(classic admins)'].principalName"
