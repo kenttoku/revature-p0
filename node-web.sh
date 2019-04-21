9e222f220b2309bb2492dd0c82d9ef2c3707c9d7
Kent Tokunaga <KentTokunaga@gmail.com>
2019-04-21 12:18:33 -0500

    Add control flow logic for commands

--- node-web.sh
+++ node-web.sh
@@ -10,3 +10,18 @@ stop()
 {
   echo "stopping app"
 }
+
+command=$1
+
+if [ -z $command ]; then
+  echo "Missing command. Please use 'start' or 'stop'"
+  exit 1
+fi
+
+if [ $command = "start" ]; then
+  start
+elif [ $command = "stop" ]; then
+  stop
+else
+  echo "Invalid command. Please use 'start' or 'stop'"
+fi
