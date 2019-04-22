dd117401709a57e032f84d092eb1178a2f7d8e32
Kent Tokunaga <KentTokunaga@gmail.com>
2019-04-22 14:50:13 -0500

    Only check for git and node

--- git-node-project.sh
+++ git-node-project.sh
@@ -6,33 +6,19 @@
 ## name: git-node-project.sh
 
 ## verify dev environment
-brew=$(which brew)
 node=$(which node)
 git=$(which git)
-az=$(which az)
-
-## check for brew
-if [ -z $brew ]; then
-  echo "No brew. Please install before continuing" 1>&2
-  exit 1
-fi
 
 ## check for node
 if [ -z $node ]; then
   echo "No node. Please install before continuing" 1>&2
-  exit 2
+  exit 1
 fi
 
 ## check for git
 if [ -z $git ]; then
   echo "No git. Please install before continuing" 1>&2
-  exit 3
-fi
-
-## check for azure-cli
-if [ -z $az ]; then
-  echo "No azure-cli. Please install before continuing" 1>&2
-  exit 4
+  exit 1
 fi
 
 ## create git project
