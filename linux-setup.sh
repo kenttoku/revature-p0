59ab45e6ee517c7c0dcd1dd21a048dde1b630dd5
Kent Tokunaga <KentTokunaga@gmail.com>
2019-04-23 20:26:55 -0500

    Remove unnecessary text from echo

--- linux-setup.sh
+++ linux-setup.sh
@@ -11,7 +11,7 @@ echo "apt update completed"
 
 ## set up brew - installation instructions from https://docs.brew.sh/Homebrew-on-Linux
 echo "brew install started"
-echo "/n" | sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+echo "" | sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
 test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
